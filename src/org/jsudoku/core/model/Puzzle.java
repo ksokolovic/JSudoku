12aa59a98f97b317b089c90a2d0212f0fa443044
Kemal Sokolovic <kemal.sokolovic@gmail.com>
2014-05-16 00:18:18 +0200

    Initialized Puzzle model class which models a Sudoku puzzle.

---
+++ src/org/jsudoku/core/model/Puzzle.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014  Kemal Sokolović <kemal DOT sokolovic AT gmail DOT com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jsudoku.core.model;
+
+/**
+ * Class models a 9x9 Sudoku puzzle. A Sudoku puzzle is made up of 9x9 grid filled with numbers in [1..9] range.
+ * The aim of the Sudoku game is to place a number from 1 to 9 into each of the cells, such that each number
+ * must appear at least once in each row and each column in the grid. Additionally, each minigrid (nine smaller 3x3
+ * grids) must contain all the numbers 1 through 9.
+ *
+ * @author sokolovic
+ */
+public class Puzzle {
+}
