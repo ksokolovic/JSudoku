bd92369de5162c28c54fef9e9d1afcb37686834e
Kemal Sokolovic <kemal.sokolovic@gmail.com>
2014-05-16 17:25:29 +0200

    Initialized couple of representative puzzle to test the model class against.

--- test/org/jsudoku/core/model/PuzzleTest.java
+++ test/org/jsudoku/core/model/PuzzleTest.java
@@ -22,12 +22,93 @@
 package org.jsudoku.core.model;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Unit test class to test the behavior of the <code>Puzzle</code> model class.
+ *
  * @author sokolovic
  */
 public class PuzzleTest extends TestCase {
+    /**
+     * Helper method to initialize various puzzles used for testing before each test is executed.
+     * @throws Exception if an error occurred during the execution.
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        initializeEmptyPuzzle();
+        initializeUnsolvedPuzzle();
+        initializeValidSolvedPuzzle();
+        initializeInvalidSolvedPuzzle();
+    }
+
+    /**
+     * Initializes an empty (all zero values) puzzle.
+     */
+    private void initializeEmptyPuzzle()
+    {
+        emptyPuzzle = new Puzzle();
+    }
+
+    /**
+     * Initializes a puzzle which is not yet solved but is valid (no invalid moves have been made).
+     */
+    private void initializeUnsolvedPuzzle()
+    {
+        int[][] puzzle = new int[][] {
+            {9, 0, 8, 1, 6, 2, 5, 0, 0},
+            {0, 3, 1, 8, 0, 0, 0, 0, 0},
+            {0, 6, 0, 3, 5, 0, 9, 8, 0},
+            {0, 1, 0, 9, 0, 0, 0, 7, 6},
+            {0, 0, 6, 0, 0, 0, 1, 0, 0},
+            {2, 5, 0, 0, 0, 1, 0, 9, 0},
+            {0, 4, 3, 0, 9, 8, 0, 2, 0},
+            {0, 0, 0, 0, 0, 6, 3, 1, 0},
+            {0, 0, 9, 7, 1, 3, 8, 0, 4}
+        };
+        unsolvedPuzzle = new Puzzle(puzzle);
+    }
+
+    /**
+     * Initializes a correctly solved puzzle.
+     */
+    private void initializeValidSolvedPuzzle()
+    {
+        int[][] puzzle = new int[][] {
+            {9, 5, 4, 3, 8, 2, 1, 7, 6},
+            {7, 3, 6, 1, 9, 5, 4, 8, 2},
+            {8, 1, 2, 4, 6, 7, 3, 5, 9},
+            {1, 8, 3, 9, 2, 6, 5, 4, 7},
+            {6, 4, 5, 8, 7, 3, 9, 2, 1},
+            {2, 9, 7, 5, 4, 1, 8, 6, 3},
+            {5, 7, 9, 2, 1, 4, 6, 3, 8},
+            {4, 6, 8, 7, 3, 9, 2, 1, 5},
+            {3, 2, 1, 6, 5, 8, 7, 9, 4}
+        };
+        validSolvedPuzzle = new Puzzle(puzzle);
+    }
+
+    /**
+     * Initializes an incorrectly solved puzzle (invalid moves have been made).
+     */
+    private void initializeInvalidSolvedPuzzle()
+    {
+        int[][] puzzle = new int[][] {
+            {9, 5, 4, 3, 8, 2, 1, 7, 6},
+            {7, 3, 6, 1, 9, 5, 4, 8, 2},
+            {8, 1, 2, 4, 6, 7, 3, 5, 9},
+            {1, 8, 3, 9, 2, 6, 5, 4, 7},
+            {6, 4, 5, 8, 7, 3, 9, 2, 1},
+            {2, 9, 7, 5, 4, 1, 8, 6, 3},
+            {5, 7, 9, 2, 1, 4, 6, 3, 8},
+            {4, 6, 8, 7, 3, 9, 2, 1, 5},
+            {3, 2, 1, 6, 5, 8, 7, 9, 4}
+        };
+        invalidSolvedPuzzle = new Puzzle(puzzle);
+    }
+
     @Test
     public void testGetActualAt() throws Exception
     {
@@ -69,4 +150,9 @@ public class PuzzleTest extends TestCase {
     {
 
     }
+
+    Puzzle emptyPuzzle;                 // Puzzle with all empty cells
+    Puzzle unsolvedPuzzle;              // Unsolved puzzle with some cells filled with valid values
+    Puzzle validSolvedPuzzle;           // Completely correctly solved puzzle
+    Puzzle invalidSolvedPuzzle;         // Invalidly solved puzzle
 }
