22e5e5aea39731091ea37d5db09bd8cd20263ae4
Kemal Sokolovic <kemal.sokolovic@gmail.com>
2014-05-27 15:33:14 +0200

    This is basically the sketch of an algorithm to be used for generating a new puzzle. The source needs to be refactored later, if this concept is right, since now it's a mess. The TODO method left empty is the one that has to be implemented so the generator is able to make symetrical puzzles.

--- src/org/jsudoku/core/generator/Generator.java
+++ src/org/jsudoku/core/generator/Generator.java
@@ -24,6 +24,8 @@ package org.jsudoku.core.generator;
 import org.jsudoku.core.model.Puzzle;
 import org.jsudoku.core.solver.Solver;
 
+import java.util.Random;
+
 /**
  * The class generates the new Sudoku puzzle based on the given level of difficulty.
  *
@@ -86,10 +88,158 @@ public class Generator {
         } while(true);
     }
 
+    /**
+     * Method generates new Sudoku puzzle based on the selected level of
+     * difficulty. It returns a puzzle as a string of numbers.
+     * @param level Level of difficulty of the puzzle being generated.
+     * @return Sudoku puzzle as string of numbers.
+     */
     private String generateNewPuzzle(int level)
     {
-        // TODO
-        return null;
+        puzzle = new Puzzle();
+        solver = new Solver(puzzle);
+        int col = 0;
+        int row = 0;
+        String str = null;
+        int numberOfEmptyCells = 0;
+
+        // Initialize entire board
+        for(row = 0; row < 9; ++row)
+        {
+            for(col = 0; col < 9; ++col)
+            {
+                puzzle.setActualAt(row, col, 0);
+                puzzle.setPossibleAt(row, col, "");
+            }
+        }
+        // Clear the stacks
+        puzzle.clearActualStack();
+        puzzle.clearPossibleStack();
+
+        // Populate the board with numbers by solving an empty grid
+        try
+        {
+            // Use logical methods to setup the grid first
+            if(!solver.solve())
+            {
+                // If it doesn't work, use brute force
+                solver.solvePuzzleByBruteForce();
+            }
+        }
+        catch(Exception exception)
+        {
+            // Just in case an error occured, return an empty string
+            return "";
+        }
+
+        // Make a backup of an actual array
+        puzzle.setActualBackup(puzzle.getActual().clone());
+
+        // Set the number of empty cells based on the selected level of difficulty
+        numberOfEmptyCells = getNumberOfEmptyCells(level);
+
+        // Clear the stacks that are used in brute force elimination
+        puzzle.clearActualStack();
+        puzzle.clearPossibleStack();
+        solver.setBruteForceStop(false);
+
+        // Create empty cells
+        createEmptyCells(numberOfEmptyCells);
+
+        // Convert the values in the actual array into string
+        str = "";
+        for(row = 0; row < 9; ++row)
+        {
+            for(col = 0; col < 9; ++col)
+            {
+                str += String.valueOf(puzzle.getActualAt(row, col));
+            }
+        }
+
+        // Ensure that the puzzle has one solution
+        do
+        {
+            puzzle.setTotalScore(0);
+            try
+            {
+                if(!solver.solve())
+                {
+                    // If puzzle is not solved and this is a level 1 - 3 puzzle
+                    if(level < 4)
+                    {
+                        // Try generation again
+                        return "";
+                    }
+                    else
+                    {
+                        // Level 4 puzzles don't guarantee single solution
+                        // and potentially need guessing
+                        solver.solvePuzzleByBruteForce();
+                        break;
+                    }
+                }
+                else
+                {
+                    // Puzzle does indeed have one solution
+                    break;
+                }
+            }
+            catch(Exception exception)
+            {
+                return "";
+            }
+        } while(true);
+
+        // Return the puzzle as string
+        return str;
+    }
+
+    /**
+     * Method returns a random number of cells to be set as empty based on the
+     * level of difficulty of the puzzle.
+     * @param level Level of difficulty of the puzzle being generated.
+     * @return Random number of cells to be left empty.
+     */
+    private int getNumberOfEmptyCells(int level)
+    {
+        switch(level)
+        {
+            case 1:
+                return randomNumber(40, 45);
+            case 2:
+                return randomNumber(46, 49);
+            case 3:
+                return randomNumber(50, 53);
+            case 4:
+                return randomNumber(54, 58);
+            default:
+                return -1;
+        }
+    }
+
+    /**
+     * Method returns a random integer number within the range specified with
+     * min and max value.
+     * @param min Minimum value of the random number.
+     * @param max Maximum value of the random number.
+     * @return Random number within the [min, max] range.
+     */
+    private int randomNumber(int min, int max)
+    {
+        Random random = new Random();
+        return (int)((max - min + 1) * random.nextDouble()) + min;
+    }
+
+    /**
+     * Method randomly determines the location of empty cells in the Sudoku grid.
+     * It first generates the location of empty cells in the top left half of
+     * the grid. It then "reflects" the empty cells onto the bottom half of the
+     * grid so that a symmetrical Sudoku puzzle is achieved.
+     * @param number Number of cells to be left empty.
+     */
+    private void createEmptyCells(int number)
+    {
+        // TODO 
     }
 
     private Solver solver;          // The solver that we use to create the puzzle in reverse
