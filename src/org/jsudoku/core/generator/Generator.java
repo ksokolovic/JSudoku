b0821f1662f65202db7178d06f38db2f8d92751f
Kemal Sokolovic <kemal.sokolovic@gmail.com>
2014-05-27 14:49:38 +0200

    The generator class initialized with a public method that is going to be used for generating a new puzzle based on the selected level of difficulty.

--- src/org/jsudoku/core/generator/Generator.java
+++ src/org/jsudoku/core/generator/Generator.java
@@ -21,10 +21,78 @@
  */
 package org.jsudoku.core.generator;
 
+import org.jsudoku.core.model.Puzzle;
+import org.jsudoku.core.solver.Solver;
+
 /**
  * The class generates the new Sudoku puzzle based on the given level of difficulty.
  *
  * @author sokolovic
  */
 public class Generator {
+
+    /**
+     * This method calls the <code>generateNewPuzzle()</code> method to ensure that
+     * the puzzle is of the right level of difficulty. It does so by checking
+     * the score of the puzzle and then generating a new puzzle if the score
+     * does not match the level of difficulty required.
+     * @param level Level of difficulty of the puzzle being generated.
+     * @return String containing values of the newly generated puzzle.
+     */
+    public String generatePuzzle(int level)
+    {
+        int score = 0;
+        String result = null;
+
+        do
+        {
+            result = generateNewPuzzle(level);
+            score = puzzle.getTotalScore();
+            if(!"".equals(result))
+            {
+                // Check if the puzzle matches the level of difficulty
+                switch(level)
+                {
+                    // Average for this level is 45
+                    case 1:
+                        if(score >= 41 && score <= 50)
+                        {
+                            return result;
+                        }
+                        break;
+                    // Average for this level is 55
+                    case 2:
+                        if(score >= 51 && score <= 60)
+                        {
+                            return result;
+                        }
+                        break;
+                    // Average for this level is 65
+                    case 3:
+                        if(score >= 61 && score <= 70)
+                        {
+                            return result;
+                        }
+                        break;
+                    // Average for this level is 115
+                    case 4:
+                        if(score >= 111 && score <= 120)
+                        {
+                            return result;
+                        }
+                        break;
+                }
+            }
+        } while(true);
+    }
+
+    private String generateNewPuzzle(int level)
+    {
+        // TODO
+        return null;
+    }
+
+    private Solver solver;          // The solver that we use to create the puzzle in reverse
+    private Puzzle puzzle;          // The puzzle generated
+
 }
